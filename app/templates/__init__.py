a17ee0dd14626683f9026e83290c48b3ee44f6cc
QueenZ <126025921+QueenZ215@users.noreply.github.com>
2026-04-27 21:11:11 -0700

    Feat/initial prorotype (#2)
    * basic app and file structor
    
    * feat: initial working prototype
    
    * feat: initial working prototype
    
    * feat: fixed bandit sqli in routes.py
    
    * feat: fixed bandit sqli in routes.py
    
    * feat: fixed bandit sqli in routes.py
    
    * feat: fixed bandit sqli in routes.py
    
    * feat: fixed bandit sqli in routes.py
    
    * feat: fixed docker cmd error
    
    * feat: owasp fix
    
    * feat: owasp fix
    
    * feat: owasp fix
    
    * moved __init--.py to app/templates

---
+++ app/templates/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from .database import init_db
+
+def create_app():
+    app = Flask(__name__)
+    init_db()
+    from .routes import main
+    app.register_blueprint(main)
+    return app
